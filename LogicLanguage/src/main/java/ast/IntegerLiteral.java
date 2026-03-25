f6a84a81cb49272aec949b1c57fe4bbc4c16b51d
Christopher1019 <159206925+Christopher1019@users.noreply.github.com>
2026-03-24 20:03:12 -0400

    Delete LogicLanguage/src/main directory
    Removing old directories

--- LogicLanguage/src/main/java/ast/IntegerLiteral.java
+++
@@ -1,9 +0,0 @@
-package ast;
-
-public class IntegerLiteral extends Expression {
-    public int value;
-
-    public IntegerLiteral(int value) {
-        this.value = value;
-    }
-}
--- LogicLanguage/src/main/java/ast/PrintStatement.java
+++
@@ -1,9 +0,0 @@
-package ast;
-
-public class PrintStatement extends Statement {
-    public Expression expression;
-
-    public PrintStatement(Expression expression) {
-        this.expression = expression;
-    }
-}
